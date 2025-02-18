33a7af0958ead80a0c1b3712384b59523cef9559
harshit-G51102 <harshitgangwar51102@gmail.com>
2025-02-18 17:12:47 +0530

    minor changes

--- musik/settings.py
+++ musik/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4m1(-0!=kjw2pnf+!w*vsohm=2ud=cba^xi+=*usogv8b%342x'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-secret-key")  # Use a default key if not set
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
