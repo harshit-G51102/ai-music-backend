7b8aadcdcc78853ecef76d48774c89a9bf365dd5
harshit-G51102 <harshitgangwar51102@gmail.com>
2025-02-18 17:10:02 +0530

    some minor changes

--- musik/settings.py
+++ musik/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4m1(-0!=kjw2pnf+!w*vsohm=2ud=cba^xi+=*usogv8b%342x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
