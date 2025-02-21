1b0cd1a382029d614b60dd80f1089ec31b4f6719
harshit-G51102 <harshitgangwar51102@gmail.com>
2025-02-21 18:31:06 +0530

    adding corsheaders

--- musik/settings.py
+++ musik/settings.py
@@ -48,9 +48,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'main',
+    'corsheaders',  
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,6 +61,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",  
+]
+
 
 ROOT_URLCONF = 'musik.urls'
 
