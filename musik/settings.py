347ce651956fd8355684bbabc30eb4da4fd1617d
harshit-G51102 <harshitgangwar51102@gmail.com>
2025-02-21 19:09:19 +0530

    adding cors validation

--- musik/settings.py
+++ musik/settings.py
@@ -62,10 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5173",  
-]
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'musik.urls'
 
